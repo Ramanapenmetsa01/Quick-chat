bcf08bfe52e5336a199acfd5f352dfc7e2f623b6
Ramanapenmetsa01 <ramanavarma9999@gmail.com>
2025-11-14 15:37:45 +0530

    Add token header to CORS allowed headers

--- server/server.js
+++ server/server.js
@@ -57,7 +57,7 @@ app.use(cors({
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization', 'token']
 }))
 
 
